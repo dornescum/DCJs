86439b68a9571e8d62801e4cbf1e14e7ad213237
Mihai Dornescu <dornescu@gmail.com>
2021-05-10 18:11:19 +0300

    oficii birouri main  async await mock data

--- FirmeCadastruJavaScript/js/data/oficiiCadastru/oficiiCadstruMain.js
+++ FirmeCadastruJavaScript/js/data/oficiiCadastru/oficiiCadstruMain.js
@@ -1,26 +1,135 @@
 let linkOficii = document.getElementById("test")
 
-fetch("./data/oficiiCadastruJudete.json")
-    .then((response)=>{
-        return  response.json()
-         console.log("resolve", response)
-     }).then(data=>{
-    // console.log(data)
+// fetch("./data/oficiiCadastruJudete.json")
+//     .then((response)=>{
+//         return  response.json()
+//          console.log("resolve", response)
+//      }).then(data=>{
+//     // console.log(data)
+//     linkOficii.innerHTML =data.map((item)=>{
+//         const {judet, adresa, coordonate, telefon, email, fax, website, programAutorizati, programPublic, link} = item;
+//         return `
+//             <div class="container" >
+//             <div class="listing-detail margin-50px-bottom sm-margin-35px-bottom">
+//                 <h3 class="font-weight-900 border-bottom text-center">Oficii si birouri de cadastru din judetul
+//                     <code>${judet}</code></h3>
+//             </div>
+//             <div class="margin-10px-bottom">
+//                 <a href="${link}" class="font-weight-bold margin-10px-right text-theme-color">
+//                     <i class="fas fa-arrow-alt-circle-right"></i>
+//                     Lista firmelor de cadastru din judetul ${judet}</a>
+//             </div>
+//             <div><h6>
+//                 Oficiul de cadastru si publicitate imobiliara ${judet}
+//             </h6></div>
+//             <div class="row">
+//                 <div class="col-lg-8 margin-10px-bottom pt-1">
+//                     <table class="table table-hover table-bordered">
+//                         <thead>
+//                         <tr class="bg-light-gray">
+//                             <td colspan="2"><i class="fa fa-address-book margin-10px-right text-theme-color padding-25px-left"
+//                                    aria-hidden="true"></i>Date de contact
+//                             </td>
+//                         </tr>
+//                         </thead>
+//                         <tbody>
+//                         <tr>
+//                             <td class="pl-3">Adresa</td>
+//                             <td class="pl-3">${adresa}
+//                                 <a href="#"  data-tool-tip="${coordonate}">
+//                                     <i class="fa fa-map-marked-alt text-theme-color"></i>
+//                                 </a>
+//                             </td>
+//                         </tr>
+//                         <tr>
+//                             <td class="pl-3">Telefon</td>
+//                             <td class="pl-3"> ${telefon}</td>
+//                         </tr>
+//                         <tr>
+//                             <td class="pl-3">Email</td>
+//                             <td class="pl-3"><a href="" class="text-theme-color"> ${email}</a></td>
+//                         </tr>
+//                         <tr>
+//                             <td class="pl-3">Fax</td>
+//                             <td class="pl-3">${fax}</td>
+//                         </tr>
+//                         <tr>
+//                             <td class="pl-3">Website</td>
+//                             <td class="pl-3"><a href="#" class="text-theme-color">${website}</a></td>
+//                         </tr>
+//                         </tbody>
+//                     </table>
+//                 </div>
+//                 <div class="col-lg-4 pt-1">
+//                     <table class="table table-hover table-bordered">
+//                          <tr class="bg-light-gray">
+//                              <th scope="row"><i class="fas fa-calendar-alt padding-20px-left text-theme-color"
+//                                                 aria-hidden="true"></i>
+//                              </th>
+//                              <td class="pl-3"><b>Public</b></td>
+//                              <td class="pl-3"><b>Autorizati</b></td>
+//                          </tr>
+//                          <tr>
+//                              <th scope="row" class="pl-3">Luni</th>
+//                              <td class="pl-3">${programPublic.luni}</td>
+//                              <td class="pl-3">${programAutorizati.luni}</td>
+//                          </tr>
+//                          <tr>
+//                              <th scope="row" class="pl-3">Marti</th>
+//                              <td class="pl-3">${programPublic.marti}</td>
+//                              <td class="pl-3">${programAutorizati.marti}</td>
+//                          </tr>
+//                          <tr>
+//                              <th scope="row" class="pl-3">Miercuri</th>
+//                              <td class="pl-3">${programPublic.miercuri}</td>
+//                              <td class="pl-3">${programAutorizati.miercuri}</td>
+//                          </tr>
+//                          <tr>
+//                              <th scope="row" class="pl-3">Joi</th>
+//                              <td class="pl-3">${programPublic.joi}</td>
+//                              <td class="pl-3">${programAutorizati.joi}</td>
+//                          </tr>
+//                          <tr>
+//                              <th scope="row" class="pl-3">Vineri</th>
+//                              <td class="pl-3">${programPublic.vineri}</td>
+//                              <td class="pl-3">${programAutorizati.vineri}</td>
+//                          </tr>
+//                      </table>
+//                  </div>
+//              </div>
+//          </div>
+//          `
+//     }).join("")
+//      }).catch((err)=>{
+//     console.log("reject", err)})
+
+
+// export default linkOficii
+
+const OCM ='./data/data.json';
+const getInfoBi = async ()=>{
+    const response = await fetch(OCM);
+    const data = await response.json();
+    return data;
+}
+
+getInfoBi().then((data)=>{
+    console.log(data)
     linkOficii.innerHTML =data.map((item)=>{
-        const {judet, adresa, coordonate, telefon, email, fax, website, programAutorizati, programPublic, link} = item;
+        const {country, adress, phone, email, town, link} = item;
         return `
             <div class="container" >
             <div class="listing-detail margin-50px-bottom sm-margin-35px-bottom">
                 <h3 class="font-weight-900 border-bottom text-center">Oficii si birouri de cadastru din judetul
-                    <code>${judet}</code></h3>
+                    <code>${town}</code></h3>
             </div>
             <div class="margin-10px-bottom">
                 <a href="${link}" class="font-weight-bold margin-10px-right text-theme-color">
                     <i class="fas fa-arrow-alt-circle-right"></i>
-                    Lista firmelor de cadastru din judetul ${judet}</a>
+                    Lista firmelor de cadastru din judetul ${town}</a>
             </div>
             <div><h6>
-                Oficiul de cadastru si publicitate imobiliara ${judet}
+                Oficiul de cadastru si publicitate imobiliara ${country}
             </h6></div>
             <div class="row">
                 <div class="col-lg-8 margin-10px-bottom pt-1">
@@ -35,15 +144,15 @@ fetch("./data/oficiiCadastruJudete.json")
                         <tbody>
                         <tr>
                             <td class="pl-3">Adresa</td>
-                            <td class="pl-3">${adresa}
-                                <a href="#"  data-tool-tip="${coordonate}">
+                            <td class="pl-3">${adress}
+                                <a href="#"  data-tool-tip="${town}">
                                     <i class="fa fa-map-marked-alt text-theme-color"></i>
                                 </a>
                             </td>
                         </tr>
                         <tr>
                             <td class="pl-3">Telefon</td>
-                            <td class="pl-3"> ${telefon}</td>
+                            <td class="pl-3"> ${phone}</td>
                         </tr>
                         <tr>
                             <td class="pl-3">Email</td>
@@ -51,11 +160,11 @@ fetch("./data/oficiiCadastruJudete.json")
                         </tr>
                         <tr>
                             <td class="pl-3">Fax</td>
-                            <td class="pl-3">${fax}</td>
+                            <td class="pl-3">${email}</td>
                         </tr>
                         <tr>
                             <td class="pl-3">Website</td>
-                            <td class="pl-3"><a href="#" class="text-theme-color">${website}</a></td>
+                            <td class="pl-3"><a href="#" class="text-theme-color">${email}</a></td>
                         </tr>
                         </tbody>
                     </table>
@@ -71,28 +180,28 @@ fetch("./data/oficiiCadastruJudete.json")
                          </tr>
                          <tr>
                              <th scope="row" class="pl-3">Luni</th>
-                             <td class="pl-3">${programPublic.luni}</td>
-                             <td class="pl-3">${programAutorizati.luni}</td>
+                             <td class="pl-3"></td>
+                             <td class="pl-3"></td>
                          </tr>
                          <tr>
                              <th scope="row" class="pl-3">Marti</th>
-                             <td class="pl-3">${programPublic.marti}</td>
-                             <td class="pl-3">${programAutorizati.marti}</td>
+                             <td class="pl-3"></td>
+                             <td class="pl-3"></td>
                          </tr>
                          <tr>
                              <th scope="row" class="pl-3">Miercuri</th>
-                             <td class="pl-3">${programPublic.miercuri}</td>
-                             <td class="pl-3">${programAutorizati.miercuri}</td>
+                             <td class="pl-3"></td>
+                             <td class="pl-3"></td>
                          </tr>
                          <tr>
                              <th scope="row" class="pl-3">Joi</th>
-                             <td class="pl-3">${programPublic.joi}</td>
-                             <td class="pl-3">${programAutorizati.joi}</td>
+                             <td class="pl-3"></td>
+                             <td class="pl-3"></td>
                          </tr>
                          <tr>
                              <th scope="row" class="pl-3">Vineri</th>
-                             <td class="pl-3">${programPublic.vineri}</td>
-                             <td class="pl-3">${programAutorizati.vineri}</td>
+                             <td class="pl-3"></td>
+                             <td class="pl-3"></td>
                          </tr>
                      </table>
                  </div>
@@ -100,12 +209,4 @@ fetch("./data/oficiiCadastruJudete.json")
          </div>
          `
     }).join("")
-     }).catch((err)=>{
-    console.log("reject", err)})
-
-
-// export default linkOficii
-
-var str = "Hello world, welcome to the universe. mam";
-var n = str.includes("mama");
-console.log(n)
+}).catch(err => console.log(err))
